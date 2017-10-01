09bcd12bc5cf30c2af5696ce24fc2a6f17c26618
Mitchel Severe <truth531@gmail.com>
2017-10-01 09:29:51 -0400

    add mongoose connection and db, and body parser middleware

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const articlesController = require('./controllers/articles-controller');
+
 // initialize app
 const app = express();
-const path = require('path');
 const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost/nodekb', {
+  useMongoClient: true,
+});
+const db = mongoose.connection;
+// check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+})
+// check for errors
+db.on('error', err => {
+  console.log(err);
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 // set view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 // start server
 app.listen(port, () => {
   console.log('Server started on port', port);
 });
-// get home route
-app.get('/', (req, res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Article One',
-      author: 'Brad Traversy',
-      body: 'This is article one',
-    },
-    {
-      id: 2,
-      title: 'Article Two',
-      author: 'Mitchel Severe',
-      body: 'This is article two',
-    },
-    {
-      id: 3,
-      title: 'Article One',
-      author: 'Winston Churchill',
-      body: 'This is article three',
-    },
-  ];
-  res.render('index', {
-    title: 'Articles',
-    articles: articles,
-  });
-});
 
-app.get('/articles/add', (req, res) => {
-  res.render('add_article', {
-    title: 'Add Articles',
-  });
-});
+// get home route
+app.get('/', articlesController.index);
+
+const articleRoutes = require('./routes/routes');
+app.use('/', articleRoutes);
+
+
 
