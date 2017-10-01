cf236d1da43bbd35066f68702945b43e258c78e0
Mitchel Severe <truth531@gmail.com>
2017-09-30 21:42:25 -0400

    add pug view engine

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
+// initialize app
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
-
+// set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// start server
 app.listen(port, () => {
   console.log('Server started on port', port);
 });
-
+// get home route
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
