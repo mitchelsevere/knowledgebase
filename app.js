04a27ab99b6a241560762cccb979549484fa547d
Mitchel Severe <truth531@gmail.com>
2017-09-30 22:09:29 -0400

    test sending data to views

--- app.js
+++ app.js
@@ -12,5 +12,35 @@ app.listen(port, () => {
 });
 // get home route
 app.get('/', (req, res) => {
-  res.render('index');
+  let articles = [
+    {
+      id: 1,
+      title: 'Article One',
+      author: 'Brad Traversy',
+      body: 'This is article one',
+    },
+    {
+      id: 2,
+      title: 'Article Two',
+      author: 'Mitchel Severe',
+      body: 'This is article two',
+    },
+    {
+      id: 3,
+      title: 'Article One',
+      author: 'Winston Churchill',
+      body: 'This is article three',
+    },
+  ];
+  res.render('index', {
+    title: 'Articles',
+    articles: articles,
+  });
 });
+
+app.get('/articles/add', (req, res) => {
+  res.render('add_article', {
+    title: 'Add Articles',
+  });
+});
+
