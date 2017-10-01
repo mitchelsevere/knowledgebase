3fa86b87bf1944d296eae28e77e0b4df5772838e
Mitchel Severe <truth531@gmail.com>
2017-10-01 11:59:17 -0400

    add new and update methods

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -3,12 +3,10 @@ const articleRouter = express.Router();
 
 const articlesController = require('../controllers/articles-controller');
 
-articleRouter.get('/add', (req, res) => {
-  res.render('add-article', {
-    title: 'Add Articles',
-  });
-});
-
+articleRouter.get('/add', articlesController.new);
 articleRouter.post('/add', articlesController.create);
+articleRouter.get('/:id', articlesController.show);
+articleRouter.get('/edit/:id', articlesController.edit);
+articleRouter.post('/edit/:id', articlesController.update);
 
 module.exports = articleRouter;
